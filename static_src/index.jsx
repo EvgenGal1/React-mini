8fadb17c7b119da01c05dda66798cca79085d762
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2020-11-15 01:03:12 +0300

    2. начало

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import index_les from '/indexs/index_les.jsx';
-import index_mes from "/indexs/index_mes.jsx";
+//?  Почему то не работает вместе с App. Перебивают его
+// import '/indexs/index_les.jsx';
+// import "/indexs/index_mes.jsx";
+//?  Почему то не работает вместе с indexs. Перебивается ими
 import App from './components/App.jsx';
 ReactDOM.render(
 	<App />,
-	document.getElementById('root') ,
+	document.getElementById('root_4') ,
 );
 

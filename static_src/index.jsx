2ac894398f420dfdab75b582742db4b43f51e6d4
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2020-11-15 00:47:02 +0300

    2. начало

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,4 +1,11 @@
-import "/indexs/index_les.jsx";
-import "/indexs/index_mes.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import index_les from '/indexs/index_les.jsx';
+import index_mes from "/indexs/index_mes.jsx";
+import App from './components/App.jsx';
+ReactDOM.render(
+	<App />,
+	document.getElementById('root') ,
+);
 
--- static_src/indexs/index_mes.jsx
+++ static_src/indexs/index_mes.jsx
@@ -2,9 +2,9 @@
 let messages = ["Привет", "Как дела?"];
 const MessageComponent = (props) => <div>{props.text}</div>;
 const MessageField = (props) => {
-  return props.messages.map((message) => <MessageComponent text={message} />);
+	return props.messages.map((message) => <MessageComponent text={message} />);
 };
 ReactDOM.render(
-  <MessageField messages={messages} />,
-  document.getElementById("root_3")
+	<MessageField messages={messages} />,
+	document.getElementById("root_3")
 );

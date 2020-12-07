1acf1256c221c5a400754e3f9a01b4c75275dc53
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2020-12-07 23:40:01 +0300

    2.13.2

--- static_src/components/App.jsx
+++ static_src/components/App.jsx
@@ -1,49 +1,50 @@
 import React from 'react';
 // import ReactDOM from "react-dom";
-// 2. импорт компонента Child
+// 2.. импорт компонента Child
 import Child from './Child.jsx';
 
 // 2. Используется export default вместо просто export. Продолжение в index.jsx....
 export default class App extends React.Component {
 	state = {
 		text: 'Наш первый React-компонент',
-		// 2. После Child.jsx
+		// 2.. После Child.jsx
 		counter: 0,
 
 	};
 
-	// 2. хуки. перед отрисовкой
+	// 2.7 хуки. перед отрисовкой
 	componentWillMount() {
 		console.log('componentWillMount');
 	}
-	// 2. хуки. после первой отрисовки
+	// 2.7 хуки. после первой отрисовки
 	componentDidMount() {
 		console.log('componentDidMount');
+		// 2.7 state переносим из render в componentDidMount
 		setTimeout(() => this.setState({ 'text': 'Обновленный React-компонент' }),
 			1000);
 	}
-	// 2. хуки. после обновления и отрисовки
+	// 2.7 хуки. после обновления и отрисовки
 	componentDidUpdate() {
 		console.log('componentDidUpdate');
 	}
 
-	// 2. После Child.jsx. 
+	// 2.9 После Child.jsx. 
 	// Стрелочная ф-ия для использ контеста компонента
 	handleClick = () => {
 		this.setState({ 'counter': this.state.counter + 1 })
 	};
 
-	// 2. В классе компонента обязательно должна быть реализована функция render(). 
+	// 2.2 В классе компонента обязательно должна быть реализована функция render(). 
 	// То, что возвращает эта функция, как раз и выводится на экран.
 	render() {
 		// 2. после хуков
 		console.log('render');
-		// 2. до хуков
+		// 2.5 до хуков
 		// setTimeout(() => this.setState({ 'text': 'Обновленный React-компонент' }), 1000);
 		return (
-			// 2. До Child.jsx
+			// 2.3 До Child.jsx
 			// <h1>{this.state.text}</h1>
-			// 2. После Child.jsx
+			// 2.9 После Child.jsx
 			<div>
 				<h1>{this.state.text}</h1>
 				<Child counter={this.state.counter} />
--- static_src/components/Child.jsx
+++ static_src/components/Child.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// 2.
+// 2.8
 export default class Child extends React.Component {
+	// 2.12 Для контроля типов передаваемых в компонент props’ ов можно использовать стороннюю библиотеку PropTypes
 	static propTypes = {
 		counter: PropTypes.number,
 	};
+	//? 2.12.1 ХЗ ВООБЩЕ ЧТО И КАК
+	defaultProps = {
+		counter: 0,
+	};
+	// 2.8 хуки. перед отрисовкой
 	componentWillMount() {
 		console.log('Child componentWillMount');
 	}
@@ -16,11 +23,11 @@ export default class Child extends React.Component {
 	}
 	render() {
 		console.log('Child render');
-		// 2. до props
+		// 2.8 до props
 		// return (
 		// 	<h1>Это компонент Child</h1>
 		// )
-		// 2. Получаем переданый props
+		// 2.11 Получаем переданый props (в виде счетчик)
 		return (
 			<h1>Это компонент Child, counter: { this.props.counter}</h1>
 		)
